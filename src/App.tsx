127e7658428b04b17320628e4a084b11b1a7881a
abluedev <alexbluedev@gmail.com>
2024-08-01 22:16:23 +0200

    feat: create task card

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css'
 import {Header} from "./components/header/header.tsx";
 import {Title} from "./components/title/title.tsx";
+import {TaskCard} from "./components/task-card/task-card.tsx";
 
 function App() {
 
   return (
-    <>
+    <main className="p-20">
         <Header>
             <Title className="p-4">
                 <span className="text-4xl font-bold"> Panel </span>
             </Title>
         </Header>
-    </>
+        <section className="mt-28">
+            <TaskCard title="Create task card" resume="Create an example of Task Card component" tags={['Software Design','Feature', 'Component']} date={new Date().toDateString()} inCharge={['Lidia']}/>
+
+        </section>
+    </main>
   )
 }
 
--- src/components/task-card/task-card.tsx
+++ src/components/task-card/task-card.tsx
@@ -1,3 +1,12 @@
-export const TaskCard = () => {
-
+export const TaskCard = ({ title, tags, date, inCharge}: {title: string, resume: string, tags: string[], date: string, inCharge: string[]}) => {
+    return (<div className="p-6 border border-slate-200 rounded-md hover:border-slate-400 hover:cursor-pointer hover:bg-slate-50 flex flex-wrap gap-2 w-96">
+        <div className="flex w-full items-center">
+            <p className="text-xl font-semibold">{title} </p>
+            <p className="text-xs ml-auto text-slate-300"> { date }</p>
+        </div>
+        { tags.map( tag => <p className="text-sm rounded-md bg-violet-100 p-2 text-violet-400 mb-2"> { tag }</p>)}
+        <div className="flex gap-6 flex-wrap">
+            { inCharge.map( (charge, index) => <p key={`charge-${index}`}> { charge }</p>)}
+        </div>
+            </div>)
 }
