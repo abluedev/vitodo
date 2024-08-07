ff9a7369e0fac735af32c27ace0877a235816b7f
abluedev <alexbluedev@gmail.com>
2024-08-07 19:07:53 +0200

    feature: improve state configuration

--- src/features/State/domain/State.ts
+++ src/features/State/domain/State.ts
@@ -1,6 +1,27 @@
 export type StateProps = 'backlog' | 'to develop' | 'in progress' | 'blocked' |'done'
 
+export interface States {
+    stateName: StateProps,
+    configuration: {
+        color: string;
+    }
+}
 
-export const GetStates = (): StateProps[] => {
-    return  ['backlog', 'to develop', 'in progress', 'blocked', 'done']
+export const GetStates = (): States[] => {
+    return  [
+        {   stateName: 'backlog',
+            configuration: {color: 'text-lime-600'}},
+        {
+            stateName: 'to develop',
+            configuration: {color: 'text-emerald-600'}},
+        {
+            stateName: 'in progress',
+            configuration: {color: 'text-cyan-600'}},
+        {
+            stateName: 'blocked',
+            configuration: {color: 'text-violet-600'}},
+        {
+            stateName: 'done',
+            configuration: {color: 'text-fuchsia-600'}}
+        ]
 }
--- src/features/State/ui/state.component.tsx
+++ src/features/State/ui/state.component.tsx
@@ -1,6 +1,6 @@
-import {StateProps} from "../domain/State.ts";
+import {States} from "../domain/State.ts";
 
-export const State = ({state}: { state: StateProps }) => {
-    return <p className="uppercase">{state}</p>
+export const State = (state:  States ) => {
+    return <p className={`${state.configuration.color}`}>{state.stateName}</p>
 
 }
